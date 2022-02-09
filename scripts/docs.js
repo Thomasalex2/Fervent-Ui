89d360db2e56ffb3e2816dc749e8d01871e99736
Thomas Alex <thomas.a@tiflabs.in>
2022-02-10 01:18:37 +0530

    - added IDs to buttons and links - made sidebar sticky - gave functionality to scroll to section for all buttons

---
+++ scripts/docs.js
@@ -0,0 +1,18 @@
+// Linking the button elements
+const top_btn = document.querySelector("#top-btn");
+const typography_lnk = document.querySelector("#typography-lnk");
+const avatar_lnk = document.querySelector("#avatar-lnk");
+const alerts_lnk = document.querySelector("#alerts-lnk");
+const badges_lnk = document.querySelector("#badges-lnk");
+const buttons_lnk = document.querySelector("#buttons-lnk");
+const cards_lnk = document.querySelector("#cards-lnk");
+const image_lnk = document.querySelector("#images-lnk");
+const forms_lnk = document.querySelector("#forms-lnk");
+const toasts_lnk = document.querySelector("#toasts-lnk");
+const sliders_lnk = document.querySelector("#sliders-lnk");
+const ratings_lnk = document.querySelector("#ratings-lnk");
+
+const scrollTo = (param) => window.scrollTo({ top: document.querySelector(`#${param.srcElement.id.split("-")[0]}-section`).offsetTop, behavior: 'smooth' });
+
+link_array = [top_btn, typography_lnk, avatar_lnk, alerts_lnk, badges_lnk, buttons_lnk, cards_lnk, image_lnk, forms_lnk, toasts_lnk, sliders_lnk, ratings_lnk];
+link_array.forEach(element => element.addEventListener("click", scrollTo));
