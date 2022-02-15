5a0c0468d38546dd1be9b6faef1aeb6765940ffc
Thomas Alex <thomas.a@tiflabs.in>
2022-02-16 01:01:22 +0530

    Added grids, lists, modal, nav and ratings Modified and refined buttons, typography Updated Js to include ratings functionality

--- scripts/docs.js
+++ scripts/docs.js
@@ -10,9 +10,33 @@ const image_lnk = document.querySelector("#images-lnk");
 const forms_lnk = document.querySelector("#forms-lnk");
 const toasts_lnk = document.querySelector("#toasts-lnk");
 const sliders_lnk = document.querySelector("#sliders-lnk");
+const modals_lnk = document.querySelector("#modals-lnk");
+const lists_lnk = document.querySelector("#lists-lnk");
+const navigation_lnk = document.querySelector("#navigation-lnk");
+const grids_lnk = document.querySelector("#grids-lnk");
 const ratings_lnk = document.querySelector("#ratings-lnk");
+
+const show_modal_btn = document.querySelector("#show-modal-btn");
+const close_modal_btn = document.querySelector("#close-modal-btn");
+const modal_element = document.querySelector("#modal-element");
+
+const ratingStars = [...document.getElementsByClassName("rating-star")];
 
 const scrollTo = (event) => window.scrollTo({ top: document.querySelector(`#${event.currentTarget.id.split("-")[0]}-section`).offsetTop, behavior: 'smooth' });
 
-link_array = [top_btn, typography_lnk, avatar_lnk, alerts_lnk, badges_lnk, buttons_lnk, cards_lnk, image_lnk, forms_lnk, toasts_lnk, sliders_lnk, ratings_lnk];
+const giveRating = (star) => {
+    let starIndex = parseInt(star.currentTarget.id.split("-")[1]);
+    ratingStars.forEach(star => parseInt(star.id.split("-")[1]) <= starIndex ? star.classList.add("checked") : star.classList.remove("checked"));
+}
+
+const showModal = () => modal_element.style.display = 'flex';
+const hideModal = () => modal_element.style.display = 'none';
+
+
+link_array = [top_btn, typography_lnk, avatar_lnk, alerts_lnk, badges_lnk, buttons_lnk, cards_lnk, image_lnk, forms_lnk, toasts_lnk, sliders_lnk, modals_lnk, lists_lnk, navigation_lnk, grids_lnk, ratings_lnk];
 link_array.forEach(element => element.addEventListener("click", scrollTo));
+
+show_modal_btn.addEventListener("click", showModal);
+close_modal_btn.addEventListener("click", hideModal);
+
+ratingStars.forEach(star => star.addEventListener("click", giveRating));
